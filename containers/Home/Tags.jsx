87eb084243619a14e246c09616e1c37666289c6d
Kaitou <kaitouj345@gmail.com>
2022-10-29 23:19:54 +0600

    tags section update

--- containers/Home/Tags.jsx
+++ containers/Home/Tags.jsx
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import TagsGallery from "../../components/TagsGallery";
 import styles from "../../styles/Home/Tags.module.css";
 
 const Tags = () => {
+  const [tag, setTag] = useState("featured");
+
   return (
     <div className={styles.container}>
       <div className="">
         <h2 className={styles.tagsText}>Trending Product</h2>
       </div>
       <div className={styles.selection_container}>
-        <button className={`${styles.selection_btn} ${styles.selected_btn}`}>
+        <button
+          className={
+            tag === "featured"
+              ? `${styles.selection_btn} ${styles.selected_btn}`
+              : `${styles.selection_btn}`
+          }
+          onClick={() => setTag("featured")}
+        >
           Featured
         </button>
-        <button className={styles.selection_btn}>Latest</button>
-        <button className={styles.selection_btn}>Trending</button>
+        <button
+          className={
+            tag === "latest"
+              ? `${styles.selection_btn} ${styles.selected_btn}`
+              : `${styles.selection_btn}`
+          }
+          onClick={() => setTag("latest")}
+        >
+          Latest
+        </button>
+        <button
+          className={
+            tag === "trending"
+              ? `${styles.selection_btn} ${styles.selected_btn}`
+              : `${styles.selection_btn}`
+          }
+          onClick={() => setTag("trending")}
+        >
+          Trending
+        </button>
       </div>
       <div className="">
-        <TagsGallery />
+        {tag === "featured" ? <TagsGallery /> : null}
+        {tag === "latest" ? <TagsGallery /> : null}
+        {tag === "trending" ? <TagsGallery /> : null}
       </div>
     </div>
   );
