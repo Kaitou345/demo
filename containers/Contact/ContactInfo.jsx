af836f1aad2905566c2a471bc52298d90fb0b993
Kaitou <kaitouj345@gmail.com>
2022-11-02 19:37:08 +0600

    bit more responsive

--- containers/Contact/ContactInfo.jsx
+++ containers/Contact/ContactInfo.jsx
@@ -13,7 +13,7 @@ const ContactInfo = () => {
         <div className={styles.entry}>
           <span className={styles.title}>Address&nbsp;&nbsp;&nbsp;&nbsp;:</span>
           <p className={styles.address}>
-            79-80 77th Rd Flushing, <br /> NY 11385
+            79-80 77th <br /> Rd Flushing, NY 11385
           </p>
         </div>
         <div className={styles.entry}>
